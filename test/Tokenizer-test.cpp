44656f291325d6f36e86050b63c995463e464f12
Roberto Maestroni <roberto.maestroni@dawandamail.com>
2014-12-09 15:00:11 +0100

    Draft tests

---
+++ test/Tokenizer-test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <sqltap/Parser.h>
+
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <cassert>
+#include <strings.h>
+
+using namespace sqltap;
+
+TEST(Parser, ctor0) {
+
+  ASSERT_TRUE(true);
+}
